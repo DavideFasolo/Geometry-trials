ec6e3d672987a2aa75634d284720456adddc517a
Davide Fasolo <codetopia@fasolodesign.it>
2021-02-05 14:59:12 +0000

    added eval feature for string input in point

--- Classes.py
+++ Classes.py
@@ -1,37 +1,37 @@
 import math as ma
+import itertools
 
 def verifica_float(var):
     try:
-        float(var)
+        float(eval(str(var)))
         return True
     except ValueError:
         return False
-
+    except SyntaxError:
+        return False
+#print(eval(str("1a")))
 class Punto:
     def __init__(self,x,y,z):
         def errore_trigger():
-            return not(
+            return  not(
                     verifica_float(x)
                 and verifica_float(y)
                 and verifica_float(z)
+                )   and (
+                   ( "Classe Punto, input non valido: {{ {},{},{} }}\n".format(x,y,z) )
+                + (( verifica_float(x) and (" ")) or ("(errX: " + '{} NaN)\n'.format(x)))
+                + (( verifica_float(y) and (" ")) or ("(errY: " + '{} NaN)\n'.format(y)))
+                + (( verifica_float(z) and (" ")) or ("(errZ: " + '{} NaN)\n'.format(z)))
             ) or False
-
-        def errore_stringa():
-            return (
-                  (( verifica_float(x) and (" ")) or ("errX "))
-                + (( verifica_float(y) and (" ")) or ("errY "))
-                + (( verifica_float(z) and (" ")) or ("errZ "))
-                )
-
-        self.x=errore_trigger() and errore_stringa() or float(x)
-        self.y=errore_trigger() and errore_stringa() or float(y)
-        self.z=errore_trigger() and errore_stringa() or float(z)
+        self.x=errore_trigger() or float(eval(str(x)))
+        self.y=errore_trigger() or float(eval(str(y)))
+        self.z=errore_trigger() or float(eval(str(z)))
         self.p='{},{},{}'.format(   self.x,
                                     self.y,
                                     self.z)
-        self.pF='{},{},{}'.format(  errore_trigger() and errore_stringa() or round(self.x,3),
-                                    errore_trigger() and errore_stringa() or round(self.y,3),
-                                    errore_trigger() and errore_stringa() or round(self.z,3)
+        self.pF='{},{},{}'.format(  errore_trigger() or round(self.x,3),
+                                    errore_trigger() or round(self.y,3),
+                                    errore_trigger() or round(self.z,3)
                                     )
         
 
@@ -59,5 +59,9 @@ class Angolo:
             and imposta_proprietà()
         )
 
-pippo=Punto(1/3,0.25,"1a")
+pippo=Punto("1/3",0.25,"1+6")
+print(pippo.x)
+print(pippo.y)
+print(pippo.z)
 print(pippo.p)
+print(pippo.pF)
